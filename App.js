ace1eb042ea75eee6265f0bd2fee89a951c57681
victorlhp <victor270196@gmail.com>
2024-02-26 16:07:03 -0300

    Add componente Cat

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Cat = () => {
+  return <Text>Hello, I am a Cat!</Text>
+}
+
+export default Cat;
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Victor</Text>
