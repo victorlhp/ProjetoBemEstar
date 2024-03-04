e33d4796366d1c71ea11f917d646e75b317eb81a
victorlhp <victor270196@gmail.com>
2024-03-04 15:19:58 -0300

    adiciona operador ternário para ano/anos

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ const getNomeCompleto = (nome, sobrenome) => {
 }
 
 const Cat = ({nome, sobrenome, idade}) => {
-  return <Text>Oi, Eu sou {getNomeCompleto(nome, sobrenome).toUpperCase()}, e eu tenho {(idade)} anos</Text>
+  let texto = (idade <=1) ? "ano" : "anos"
+  
+  return <Text>Oi, Eu sou {getNomeCompleto(nome, sobrenome).toUpperCase()}, e eu tenho {(idade)} {texto}</Text>
 }
 
 export default IFAL;
@@ -16,7 +18,7 @@ function IFAL() {
     <View style={styles.container}>
       <Cat nome="Black" sobrenome="Panterinha" idade={3} />
       <Cat nome="Garfield" sobrenome="Lasanha" idade={4}/>
-      <Cat nome="Apolo" sobrenome="God" idade={5}/>
+      <Cat nome="Apolo" sobrenome="God" idade={1}/>
       <StatusBar style="auto" />
     </View>
   );
