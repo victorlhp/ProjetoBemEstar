9de1ed91dca8f13e6f22bf88e499b1b771e1e18d
victorlhp <victor270196@gmail.com>
2024-04-09 14:25:12 -0300

    posicionamento do botão e alinhamento do texto

--- app/introducao.js
+++ app/introducao.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { View, ImageBackground, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, ImageBackground, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const TelaPersonalizada = () => {
   return (
     <View style={styles.container}>
       {/* Logomarca ao fundo com efeito de marca d'água */}
-      
+      <View style={styles.text}>
       <ImageBackground
         source={require('./../assets/logoBemEstar.png')}
         style={styles.logoBackground} imageStyle={{opacity: 0.2}}
         resizeMode="cover"
-        
       >
-        {/* Texto sobreposta à logomarca */}
-        {/* <View style={styles.textContainer}> */}
-          <Text style={styles.text}>O Bem Estar é a ferramenta que você precisa para uma pré-avaliação simples e objetiva da sua ansiedade. Com perguntas cuidadosamente elaboradas, nossa abordagem direta permite que você avalie seu estado emocional de forma rápida e eficaz. Lembre-se de que esta avaliação é apenas um ponto de partida e não substitui um diagnóstico feito por um profissional de saúde mental qualificado.</Text>
-        {/* </View> */}
+      <Text style={styles.textContainer}>O Bem Estar é a ferramenta que você precisa para uma pré-avaliação simples e objetiva da sua ansiedade. Com perguntas cuidadosamente elaboradas, nossa abordagem direta permite que você avalie seu estado emocional de forma rápida e eficaz. Lembre-se de que esta avaliação é apenas um ponto de partida e não substitui um diagnóstico feito por um profissional de saúde mental qualificado.</Text>
       </ImageBackground>
+      </View>
 
-      {/* Botão "Próximo" na parte inferior da tela */}
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Próximo</Text>
-      </TouchableOpacity>
+      
+      <View style={styles.button}>
+      <Button style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-end'}} mode="text" onPress={()=>
+      console.log('Pressed')} labelStyle={{ color: 'black', fontSize: 18}}>
+        Iniciar
+      </Button>
+      </View>
     </View>
   );
 };
@@ -42,35 +43,26 @@ const styles = StyleSheet.create({
   },
   
   textContainer: {
-    //backgroundColor: 'rgba(255, 255, 255, 0.5)', // Cor de fundo com transparência para efeito de marca d'água
-    //paddingHorizontal: 20,
-    //paddingVertical: 10,
-    //borderRadius: 10,
+    justifyContent: 'center',
+    flexDirection:'column',
+    alignItems:'center',
+    fontSize: 25,
+    fontWeight: 'bold',
+    
   },
   
   text: {
-    
-    fontSize: 25,
-    fontWeight: 'bold',
+    flex: 9,
     color: '#333', // Cor do texto
-    textAlign: 'center',
-    justifyContent: 'center',
     marginHorizontal: 15,
   },
   
-  button: {
-    //backgroundColor: '#CCCCFF',
-    //paddingVertical: 15,
-    marginHorizontal: 20,
-    marginBottom: 20,
-    borderRadius: 10,
+   button: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
   },
   
-  buttonText: {
-    fontSize: 18,
-    color: 'black',
-    textAlign: 'right',
-  },
 });
 
 export default TelaPersonalizada;
