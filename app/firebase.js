46e3a453d873b98e0ec71ad9d13122e35a79d9e4
victorlhp <victor270196@gmail.com>
2024-07-15 16:31:47 -0300

    inicio firebase

--- app/firebase.js
+++ app/firebase.js
@@ -1,11 +1,19 @@
-// Firebase Configuration
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAHO7rhIrfypj-x27HetVym-08K9YmE22U",
-    authDomain: "bem-estar-3fb2c.firebaseapp.com",
-    databaseURL: "https://bem-estar-3fb2c-default-rtdb.firebaseio.com",
-    projectId: "bem-estar-3fb2c",
-    storageBucket: "bem-estar-3fb2c.appspot.com",
-    messagingSenderId: "123743475693",
-    appId: "1:123743475693:web:36043abf2701a929eff9e4",
-    measurementId: "G-NFR3GLN5R7"
-  };
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
