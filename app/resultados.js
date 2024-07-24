ce4b72266012ce66770245df7b99f9460a27a21e
victorlhp <victor270196@gmail.com>
2024-07-24 16:34:45 -0300

    teste implementação da lógica de resultados

--- app/resultados.js
+++ app/resultados.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TelaResultados = () => {
@@ -20,8 +21,25 @@ const TelaResultados = () => {
     calcularResultado();
   }, []);
 
+  //implementação da resposta com base no resultado da escala HAD (verificar com o Robério)
+
+  if (resultado <= 7) {
+    console.log('improvável');
+  } else if (resultado > 7 && resultado <= 11) {
+    console.log('possível');
+  } else if (resultado > 11 && resultado <= 21) {
+    console.log('provável');
+  } else{
+    console.log('erro');
+  }
+
+  // -------------------------------------------------------------------------------------------------
+
   return (
     <View style={styles.container}>
+        <View style={styles.logoContainer}>
+  <Image source={require('./../assets/logoBemEstar.png')} style={styles.logo} />
+</View>
       <Text style={styles.resultadoTexto}>Seu nível de ansiedade é: {resultado}</Text>
     </View>
   );
@@ -35,9 +53,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#9ed9',
   },
   resultadoTexto: {
+    flex: 1,
     fontSize: 25,
     color: '#000000',
     textAlign: 'center',
+    marginBottom: 20,
+  },
+
+  logoContainer: {
+    alignItems: 'center',
     marginBottom: 20,
   },
 });
