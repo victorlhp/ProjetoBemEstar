cd89fbab03545a3a79edddbf20776e7f397b3481
victorlhp <victor270196@gmail.com>
2024-07-25 14:39:32 -0300

    teste autenticação via google e backup

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,5 +1,27 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAHO7rhIrfypj-x27HetVym-08K9YmE22U",
+//   authDomain: "bem-estar-3fb2c.firebaseapp.com",
+//   databaseURL: "https://bem-estar-3fb2c-default-rtdb.firebaseio.com",
+//   projectId: "bem-estar-3fb2c",
+//   storageBucket: "bem-estar-3fb2c.appspot.com",
+//   messagingSenderId: "123743475693",
+//   appId: "1:123743475693:web:36043abf2701a929eff9e4",
+//   measurementId: "G-NFR3GLN5R7"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export { app };
+
+// firebaseConfig.js (separado para evitar múltiplas inicializações)
+
+// Mova a configuração do Firebase para um arquivo separado se ainda não estiver.
+import { initializeApp } from 'firebase/app';
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHO7rhIrfypj-x27HetVym-08K9YmE22U",
@@ -12,9 +34,19 @@ const firebaseConfig = {
   measurementId: "G-NFR3GLN5R7"
 };
 
+
+
+// Inicialize o app Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app };
+
+// Inicialize o Firebase Auth
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export { auth, app };
+
 
 
 
