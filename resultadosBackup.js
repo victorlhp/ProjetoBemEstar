3e5d0b3966c0fbb2caef710370e6c6a65a583910
victorlhp <victor270196@gmail.com>
2024-08-02 13:42:58 -0300

    backup tela de resultados

--- resultadosBackup.js
+++ resultadosBackup.js
@@ -3,22 +3,29 @@ import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
+// Largura da tela para uso em estilos
 const screenWidth = Dimensions.get('window').width;
 
 const Resultados = () => {
+// Estados para armazenar a pontuação de ansiedade e depressão
   const [pontuacaoAnsiedade, setPontuacaoAnsiedade] = useState(0);
   const [pontuacaoDepressao, setPontuacaoDepressao] = useState(0);
 
   useEffect(() => {
+
+// Função para calcular a pontuação de ansiedade e depressão
     const calcularPontuacao = async () => {
       try {
+        // Recupera as respostas do armazenamento local
         const respostasJson = await AsyncStorage.getItem('respostas');
         const respostas = JSON.parse(respostasJson) || [];
 
+// Calcula a pontuação de ansiedade somando valores de índices pares
         const pontuacaoAns = respostas
           .filter((_, idx) => idx % 2 === 0)
           .reduce((acc, resposta) => acc + resposta.valor, 0);
 
+// Calcula a pontuação de depressão somando valores de índices ímpares
         const pontuacaoDep = respostas
           .filter((_, idx) => idx % 2 !== 0)
           .reduce((acc, resposta) => acc + resposta.valor, 0);
@@ -33,45 +40,46 @@ const Resultados = () => {
     calcularPontuacao();
   }, []);
 
+// Função para determinar a cor do gráfico baseado na pontuação
   const getGaugeColor = (score) => {
-    if (score <= 7) return 'green';
-    if (score <= 11) return 'yellow';
-    return 'red';
+    if (score <= 7) return 'green'; // Cor verde para pontuação baixa
+    if (score <= 11) return 'yellow'; // Cor amarela para pontuação média
+    return 'red'; // Cor vermelha para pontuação alta
   };
 
   return (
     <View style={styles.container}>
       <Image source={require('./../assets/logoBemEstar.png')} style={styles.logo} />
       <View style={styles.chartContainer}>
-        <Text style={styles.chartTitle}>Resultado de Ansiedade</Text>
+        <Text style={styles.chartTitle}>Transtorno de Ansiedade</Text>
         <AnimatedCircularProgress
           size={180}
           width={15}
-          fill={(pontuacaoAnsiedade / 21) * 100}
-          tintColor={getGaugeColor(pontuacaoAnsiedade)}
+          fill={(pontuacaoAnsiedade / 21) * 100} // Calcula a porcentagem para o gráfico
+          tintColor={getGaugeColor(pontuacaoAnsiedade)} // Define a cor do gráfico
           backgroundColor="#eeeeee"
-          lineCap="round"
-          rotation={-90}
-          arcSweepAngle={180}
+          // lineCap="round"
+          rotation={-90} // Rotação do gráfico
+          arcSweepAngle={180} // Ângulo do arco do gráfico
           style={styles.gauge}
         >
           {(fill) => (
             <Text style={styles.gaugeText}>
-              {pontuacaoAnsiedade}
+              {pontuacaoAnsiedade} 
             </Text>
           )}
         </AnimatedCircularProgress>
-        <Text style={styles.resultado}>{interpretacao(pontuacaoAnsiedade)}</Text>
+        <Text style={styles.resultado}>{interpretacaoA(pontuacaoAnsiedade)}</Text>
       </View>
       <View style={styles.chartContainer}>
-        <Text style={styles.chartTitle}>Resultado de Depressão</Text>
+        <Text style={styles.chartTitle}>Depressão</Text>
         <AnimatedCircularProgress
           size={180}
           width={15}
           fill={(pontuacaoDepressao / 21) * 100}
           tintColor={getGaugeColor(pontuacaoDepressao)}
           backgroundColor="#eeeeee"
-          lineCap="round"
+          // lineCap="round"
           rotation={-90}
           arcSweepAngle={180}
           style={styles.gauge}
@@ -82,19 +90,35 @@ const Resultados = () => {
             </Text>
           )}
         </AnimatedCircularProgress>
-        <Text style={styles.resultado}>{interpretacao(pontuacaoDepressao)}</Text>
+        <Text style={styles.resultado}>{interpretacaoD(pontuacaoDepressao)}</Text>
       </View>
     </View>
   );
 };
 
-const interpretacao = (pontuacao) => {
-  if (pontuacao >= 0 && pontuacao <= 7) return 'Improvável';
-  if (pontuacao >= 8 && pontuacao <= 11) return 'Possível';
-  if (pontuacao >= 12 && pontuacao <= 21) return 'Provável';
+// Função para interpretar a pontuação e exibir a mensagem correspondente
+// const interpretacao = (pontuacao) => {
+//   if (pontuacao >= 0 && pontuacao <= 7) return 'Improvável'; // Ansiedade/Depressão improvável
+//   if (pontuacao >= 8 && pontuacao <= 11) return 'Possível'; // Ansiedade/Depressão possível
+//   if (pontuacao >= 12 && pontuacao <= 21) return 'Provável'; // Ansiedade/Depressão provável
+//   return 'Resultado fora do intervalo';
+// };
+
+const interpretacaoA = (pontuacaoAnsiedade) => {
+  if (pontuacaoAnsiedade >= 0 && pontuacaoAnsiedade <= 7) return 'Improvável: Não há probabilidade'; // Ansiedade/Depressão improvável
+  if (pontuacaoAnsiedade >= 8 && pontuacaoAnsiedade <= 11) return 'Possível: Procure um Profissional de Saúde Mental'; // Ansiedade/Depressão possível
+  if (pontuacaoAnsiedade >= 12 && pontuacaoAnsiedade <= 21) return 'Provável: Alta Probabilidade. Procure um Especialista em saúde mental'; // Ansiedade/Depressão provável
   return 'Resultado fora do intervalo';
 };
 
+const interpretacaoD = (pontuacaoDepressao) => {
+  if (pontuacaoDepressao >= 0 && pontuacaoDepressao <= 7) return 'Improvável: Você provavelmente não apresenta sintomas significativos de depressão, mas continue cuidando do seu bem-estar emocional.'; // Ansiedade/Depressão improvável
+  if (pontuacaoDepressao >= 8 && pontuacaoDepressao <= 11) return 'Possível: Há alguns sintomas leves de depressão; considere monitorar seu estado emocional e buscar orientação se necessário.'; // Ansiedade/Depressão possível
+  if (pontuacaoDepressao >= 12 && pontuacaoDepressao <= 21) return 'Provável: Sintomas significativos de depressão estão presentes; é importante procurar avaliação e apoio profissional.'; // Ansiedade/Depressão provável
+  return 'Resultado fora do intervalo';
+};
+
+// Estilos do componente
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -102,36 +126,39 @@ const styles = StyleSheet.create({
     padding: 0,
   },
   logo: {
-    width: '100%', // Mesma proporção usada na tela de criação de conta
-    height: 250,
+    width: '100%', 
+    height: 150,
     resizeMode: 'contain',
     alignSelf: 'center',
-    marginBottom: 30,
   },
+
   chartContainer: {
     alignItems: 'center',
-    marginBottom: 30,
+    marginBottom: 75,
   },
+
   chartTitle: {
-    fontSize: 25,
+    fontSize: 30,
     fontStyle: 'italic',
     textAlign: 'center',
     marginBottom: 10,
     color: '#000000',
   },
   resultado: {
-    fontSize: 18,
+    fontSize: 15,
     fontStyle: 'italic',
     textAlign: 'center',
-    marginTop: 10,
     color: '#000000',
-  },
+},
+
   gauge: {
-    marginBottom: 20,
-  },
+    marginBottom: 10,
+},
+
   gaugeText: {
     fontSize: 24,
     color: '#000000',
+    marginBottom: 65,
   },
 });
 
